557394e76db448811e7c847d2523190a00dc65fe
Igor Korp <nsveynbern@gmail.com>
2021-04-09 19:22:15 +0400

    add parameter in gradle for run app

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     kotlin("jvm") version "1.4.10"
     application
 }
+application {
+    mainClass.set("Main")
+}
 
 group = "me.igork"
 version = "1.0-SNAPSHOT"
@@ -41,7 +44,4 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
+
